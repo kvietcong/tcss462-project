b4ded25c1d287bcfca31f50d9e9fb9c174f0370d
KV Le <32186663+kvietcong@users.noreply.github.com>
2022-12-11 03:20:57 -0800

    Use pandas for data

--- clean_data.py
+++ clean_data.py
@@ -2,74 +2,54 @@ from statistics import mean, stdev
 from json import loads, dumps
 from glob import glob
 from sys import argv
+import pandas as pd
+import numpy as np
 
 
-def get_results():
-    runtime_by_language_by_newcontainer_by_filter_by_image = {}
+def clean_data():
+    runs = []
     for run_output_filepath in glob(
         "./**/test/history/**/*.json", recursive=True
     ):
         with open(run_output_filepath) as run_output:
             run_json = loads(run_output.read())
+            runs.append(run_json)
 
-            payload = run_json["payload"] = loads(
-                run_json["payload"].replace('"; "', '", "')
-            )  # Super fragile way to fix payload output
-            filter = run_json["filter"] = payload["filter"]
-            runtime = int(run_json["runtime"])
-            newcontainer = int(run_json["newcontainer"])
-            image = payload["key"]
-            filename = run_json["lang"]
+    df = pd.DataFrame(runs)
+    df = df.rename(columns={
+        "lang": "language"
+    })
+    df = df.assign(
+        payload=lambda df: df["payload"]
+        .map(lambda payload: loads(payload.replace('"; "', '", "'))),
 
-            runtime_by_language_by_newcontainer_by_filter_by_image \
-                .setdefault(image, {}) \
-                .setdefault(filter, {}) \
-                .setdefault(newcontainer, {}) \
-                .setdefault(filename, []) \
-                .append(runtime)
+        runtime=lambda df: df["runtime"].astype(float),
+        roundTripTime=lambda df: df["roundTripTime"].astype(float),
+        newcontainer=lambda df: df["newcontainer"].astype(int),
 
-            # print(run_json)
+        filter=lambda df: df["payload"].map(lambda payload: payload["filter"]),
+        filename=lambda df: df["payload"].map(lambda payload: payload["key"]),
+        containter_state=lambda df: np.where(
+            df["newcontainer"] == 1, "cold", "hot"
+        ),
+    )
 
-    return {
-        image: {
-            filter: {
-                "cold" if newcontainer else "warm": {
-                    language: {
-                        "average_ms": mean(runtimes),
-                        "standard_deviation":
-                            stdev(runtimes) if len(runtimes) > 1 else "N/A",
-                        "sample_size": len(runtimes),
-                    }
-                    for language, runtimes
-                    in runtime_by_language.items()
-                }
-                for newcontainer, runtime_by_language
-                in runtime_by_language_by_newcontainer.items()
-            }
-            for filter, runtime_by_language_by_newcontainer
-            in runtime_by_language_by_newcontainer_by_filter.items()
-        }
-        for image, runtime_by_language_by_newcontainer_by_filter
-        in runtime_by_language_by_newcontainer_by_filter_by_image.items()
-    }
+    grouped_columns = [
+        # "cpuType",
+        "filename", "filter", "containter_state", "language",
+    ]
+    runtime_results = df.groupby(grouped_columns)["runtime"] \
+        .agg(["mean", "std", "count"])
+    runtime_results = runtime_results.rename(columns={
+        "mean": "average runtime (ms)",
+        "std": "standard deviation",
+        "count": "sample size",
+    })
+    print("RUN TIME")
+    print(runtime_results.to_string())
+
+    return df
 
 
-results = get_results()
-
-if len(argv) > 1:
-    filename = argv[1]
-    print(f"Getting values for file: {filename}")
-    results = results[filename]
-
-if len(argv) > 2:
-    filter = argv[2]
-    print(f"Getting values for filter: {filter}")
-    results = results[filter]
-
-if len(argv) > 3:
-    container_state = argv[3]
-    print(f"Getting values for {container_state} container")
-    results = results[container_state]
-
-print("Runtimes")
-print(dumps(results, indent=2))
+if __name__ == "__main__":
+    clean_data()
