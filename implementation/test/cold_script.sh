9055f667ecd80e0caf3184d5924fd78db5c31ed1
KV Le <32186663+kvietcong@users.noreply.github.com>
2022-12-12 05:07:03 -0800

    Fix nested loop

--- implementation/test/cold_script.sh
+++ implementation/test/cold_script.sh
@@ -9,22 +9,33 @@ read LOOPS
 echo -n "Enter Sleep Amount in Seconds: "
 read SLEEP_AMOUNT
 
+re='^[0-9]+$'
+if ! [[ $LOOPS =~ $re ]] ; then
+    echo "error: Loops ($LOOPS) is not a number" >&2; exit 1
+fi
+if ! [[ $SLEEP_AMOUNT =~ $re ]] ; then
+    echo "error: Sleep amount ($SLEEP_AMOUNT) is not a number" >&2; exit 1
+fi
+
 echo "Looping $LOOPS time(s)"
 echo Sleeping for $SLEEP_AMOUNT seconds between experiments
 
 for i in $(seq $LOOPS)
 do
-    if [ $EXPERIMENT == "all" ]; then
-        for EXPERIMENT
-        in "greyscale" "flipVertical" "soften" "soften x 8" "soften x 16"
+    echo
+    if [ "$EXPERIMENT" == "all" ]; then
+        for EX in "greyscale" "flipVertical" "soften" "soften x 8" "soften x 16"
         do
-            (trap 'kill 0' SIGINT; python3 faas_runner.py -f functions/imageProcessingJava.json -e "experiments/$EXPERIMENT.json" & python3 faas_runner.py -f functions/imageProcessingJavaScript.json -e "experiments/$EXPERIMENT.json" & wait)
+            echo Loop $i of $EX "(All)"
 
-            echo Sleeping $SLEEP_AMOUNT seconds after loop $i of $EXPERIMENT
+            (trap 'kill 0' SIGINT; python3 faas_runner.py -f functions/imageProcessingJava.json -e "experiments/$EX.json" & python3 faas_runner.py -f functions/imageProcessingJavaScript.json -e "experiments/$EX.json" & wait) &> /dev/null
+
+            echo Sleeping $SLEEP_AMOUNT seconds after loop $i of $EX
             sleep $SLEEP_AMOUNT
         done
     else
-        (trap 'kill 0' SIGINT; python3 faas_runner.py -f functions/imageProcessingJava.json -e "experiments/$EXPERIMENT.json" & python3 faas_runner.py -f functions/imageProcessingJavaScript.json -e "experiments/$EXPERIMENT.json" & wait)
+        echo Loop $i of $EXPERIMENT "(Single)"
+        (trap 'kill 0' SIGINT; python3 faas_runner.py -f functions/imageProcessingJava.json -e "experiments/$EXPERIMENT.json" & python3 faas_runner.py -f functions/imageProcessingJavaScript.json -e "experiments/$EXPERIMENT.json" & wait) &> /dev/null
 
         echo Sleeping $SLEEP_AMOUNT seconds after loop $i of $EXPERIMENT
         sleep $SLEEP_AMOUNT
