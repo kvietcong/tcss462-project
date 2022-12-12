803b040cbdf776ee8910c4572619f581bf0400d6
KV Le <32186663+kvietcong@users.noreply.github.com>
2022-12-12 03:49:50 -0800

    Revamp experiments

--- implementation/test/cold_script.sh
+++ implementation/test/cold_script.sh
@@ -1,9 +1,32 @@
-for i in 1 2 3 4 5 6 7 8 9 10
+#!/bin/bash
+
+echo -n "Enter Experiment or \"all\": "
+read EXPERIMENT
+
+echo -n "Enter Loop Amount: "
+read LOOPS
+
+echo -n "Enter Sleep Amount in Seconds: "
+read SLEEP_AMOUNT
+
+echo "Looping $LOOPS time(s)"
+echo Sleeping for $SLEEP_AMOUNT seconds between experiments
+
+for i in $(seq $LOOPS)
 do
-    for EXPERIMENT in "mountains" "vietfood" "deathstar" "husky"
-    do
-        python3 faas_runner.py -f functions/imageProcessing.json -e experiments/$EXPERIMENT.json
-        echo Sleeping after loop $i of $EXPERIMENT
-        sleep 1000
-    done
+    if [ $EXPERIMENT == "all" ]; then
+        for EXPERIMENT
+        in "greyscale" "flipVertical" "soften" "soften x 8" "soften x 16"
+        do
+            (trap 'kill 0' SIGINT; python3 faas_runner.py -f functions/imageProcessingJava.json -e "experiments/$EXPERIMENT.json" & python3 faas_runner.py -f functions/imageProcessingJavaScript.json -e "experiments/$EXPERIMENT.json" & wait)
+
+            echo Sleeping $SLEEP_AMOUNT seconds after loop $i of $EXPERIMENT
+            sleep $SLEEP_AMOUNT
+        done
+    else
+        (trap 'kill 0' SIGINT; python3 faas_runner.py -f functions/imageProcessingJava.json -e "experiments/$EXPERIMENT.json" & python3 faas_runner.py -f functions/imageProcessingJavaScript.json -e "experiments/$EXPERIMENT.json" & wait)
+
+        echo Sleeping $SLEEP_AMOUNT seconds after loop $i of $EXPERIMENT
+        sleep $SLEEP_AMOUNT
+    fi
 done
